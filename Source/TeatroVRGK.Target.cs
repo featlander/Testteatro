cc89032858f419a6db52c409ace58ef6b232fe5c
Ivan-Covra <42679240+covra@users.noreply.github.com>
2026-01-16 22:34:08 +0100

    starter commit
    Primer commit, proyecto sobre unreal editor compilado a 2026

---
+++ Source/TeatroVRGK.Target.cs
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TeatroVRGKTarget : TargetRules
+{
+	public TeatroVRGKTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+
+
+		ExtraModuleNames.AddRange( new string[] { "TeatroVRGK" } );
+
+
+	}
+}
---
+++ Source/TeatroVRGKEditor.Target.cs
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TeatroVRGKEditorTarget : TargetRules
+{
+	public TeatroVRGKEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+
+		ExtraModuleNames.AddRange( new string[] { "TeatroVRGK" } );
+
+
+	}
+}
